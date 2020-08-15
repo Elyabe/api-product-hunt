8e24ad1dda2dfeefbfcf04d7a04266d995194a02
elyabe <elyabe.as@gmail.com>
2020-08-15 17:06:02 -0300

    Add CORS config

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const requireDir = require('require-dir');
 
+const cors = require('cors');
+
+
 // Starting App
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Starting database
 const mongoose = require('mongoose');
