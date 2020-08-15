53c53078e18478c0cd6cf6b06aae607d55b0b8cb
elyabe <elyabe.as@gmail.com>
2020-08-15 16:12:22 -0300

    First model

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const requireDir = require('require-dir');
 
 // Starting App
 const app = express();
@@ -6,10 +7,23 @@ const app = express();
 // Starting database
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/nodeapi', 
+{ useNewUrlParser: true, 
+    useUnifiedTopology: true 
+});
+
+requireDir('./src/models');
+
+
+const Product = mongoose.model('Product');
 
 // First route
 app.get('/', function( req, res){
+    Product.create({
+        title: 'React Native',
+        description: 'Build Native apps with React',
+        url: '/'
+    })
     return res.status(200).send('Hello');
 });
 
