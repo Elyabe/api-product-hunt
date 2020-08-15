2a869a6d7ce1a04230b952da71c65bbc76a20864
elyabe <elyabe.as@gmail.com>
2020-08-15 16:05:16 -0300

    Add connect database

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
+// Starting App
 const app = express();
 
+// Starting database
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// First route
 app.get('/', function( req, res){
     return res.status(200).send('Hello');
 });
+
 
 app.listen(3001);
